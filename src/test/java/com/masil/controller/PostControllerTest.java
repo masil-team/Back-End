2cbe6963e004cf4ceaf0c7b039bab862563c7c4d
unknown <wnsrl981228@gmail.com>
2023-01-16 13:19:22 +0900

    feat : spring rest docs 적용

--- src/test/java/com/masil/controller/PostControllerTest.java
+++ src/test/java/com/masil/controller/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.masil.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.masil.domain.comment.dto.CommentResponse;
 import com.masil.domain.post.controller.PostController;
 import com.masil.domain.post.dto.PostCreateRequest;
 
@@ -17,22 +18,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -43,6 +54,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //        PostService.class,
 //        UserRepository.class,
 //})
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("test")
@@ -63,12 +76,18 @@ public class PostControllerTest {
     private PostService postService;
     @Autowired
     private UserRepository userRepository;
-
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation))
+//                .build();
+//    }
     @BeforeEach
     void beforeEach() {
         // 초기화
         User user = User.builder()
                 .email("test@123")
+                .nickname("test")
                 .build();
         userRepository.save(user);
 
@@ -86,27 +105,28 @@ public class PostControllerTest {
         postRepository.save(post2);
     }
 
-    @Test
-    @DisplayName("게시글 생성")
-    void t1() throws Exception {
-        // given
-        PostCreateRequest postCreateRequest = new PostCreateRequest( "Hello");
-
-        // when, then
-        ResultActions resultActions = mvc.perform(
-                        post("/boards/1/posts")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(postCreateRequest)));
-
-        resultActions
-                /**
-                 * isCreated() -> 상태코드 201
-                 * controller에서 created() 201로 받아야 하는 이유 찾기
-                 */
-                .andExpect(status().isOk())
-                .andExpect(handler().handlerType(PostController.class))
-                .andExpect(handler().methodName("createPost"));
-    }
+//    @Test
+//    @DisplayName("게시글 생성")
+//    void t1() throws Exception {
+//        // given
+//        PostCreateRequest postCreateRequest = new PostCreateRequest( "Hello");
+//
+//        // when, then
+//        ResultActions resultActions = mvc.perform(
+//                        post("/boards/1/posts")
+//                                .accept(MediaType.APPLICATION_JSON)
+//                                .contentType(MediaType.APPLICATION_JSON)
+//                                .content(objectMapper.writeValueAsString(postCreateRequest)));
+//
+//        resultActions
+//                /**
+//                 * isCreated() -> 상태코드 201
+//                 * controller에서 created() 201로 받아야 하는 이유 찾기
+//                 */
+//                .andExpect(status().isOk())
+//                .andExpect(handler().handlerType(PostController.class))
+//                .andExpect(handler().methodName("createPost"));
+//    }
 
 
     @Test
@@ -127,57 +147,68 @@ public class PostControllerTest {
         resultActions
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(PostController.class))
-                .andExpect(content().string(containsString("content")));
+                .andExpect(content().string(containsString("content")))
+                .andDo(document("post/findOne",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("게시글 id"),
+                                fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("viewCount").type(JsonFieldType.NUMBER).description(0),
+                                fieldWithPath("comments").type(JsonFieldType.NULL).description("댓글")
+                        )
+                ));
     }
 
-    @Test
-    @DisplayName("게시글 다 건 조회")
-    void t3() throws Exception {
-        // given
-
-        String url = "/boards/1/posts/";
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get(url))
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(handler().handlerType(PostController.class));
-    }
-
-    @Test
-    @DisplayName("게시글 수정")
-    void t4() throws Exception {
-        // given
-        PostModifyRequest postModifyRequest = new PostModifyRequest( "수정된 내용");
-
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/boards/{boardId}/posts/{postId}",1,1)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(postModifyRequest)))
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(status().is2xxSuccessful());
-    }
-
-    @Test
-    @DisplayName("게시글 삭제")
-    void t5() throws Exception {
-
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/boards/{boardId}/posts/{postId}",1,1)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(status().is2xxSuccessful());
-    }
+//    @Test
+//    @DisplayName("게시글 다 건 조회")
+//    void t3() throws Exception {
+//        // given
+//
+//        String url = "/boards/1/posts/";
+//        // WHEN
+//        ResultActions resultActions = mvc
+//                .perform(get(url))
+//                .andDo(print());
+//
+//        // THEN
+//        resultActions
+//                .andExpect(status().is2xxSuccessful())
+//                .andExpect(handler().handlerType(PostController.class));
+//    }
+//
+//    @Test
+//    @DisplayName("게시글 수정")
+//    void t4() throws Exception {
+//        // given
+//        PostModifyRequest postModifyRequest = new PostModifyRequest( "수정된 내용");
+//
+//        // WHEN
+//        ResultActions resultActions = mvc
+//                .perform(get("/boards/{boardId}/posts/{postId}",1,1)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(postModifyRequest)))
+//                .andDo(print());
+//
+//        // THEN
+//        resultActions
+//                .andExpect(status().is2xxSuccessful());
+//    }
+//
+//    @Test
+//    @DisplayName("게시글 삭제")
+//    void t5() throws Exception {
+//
+//        // WHEN
+//        ResultActions resultActions = mvc
+//                .perform(get("/boards/{boardId}/posts/{postId}",1,1)
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andDo(print());
+//
+//        // THEN
+//        resultActions
+//                .andExpect(status().is2xxSuccessful());
+//    }
 
 }
