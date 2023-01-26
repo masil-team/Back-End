952b568e24880e0b7a8091ee35f26ee6b9f052eb
kwon-yeongsik <youngsik0266@gmail.com>
2023-01-26 14:53:04 +0900

    refactor : 안쓰는 과거 주석 제거

--- src/main/java/com/masil/global/auth/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/masil/global/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -20,21 +20,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     public static final String BEARER_PREFIX = "Bearer ";
     private final JwtTokenProvider jwtTokenProvider;
 
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        // 1. Request Header 에서 JWT 토큰 추출
-////        String token = resolveToken((HttpServletRequest) request);
-//
-////        // 2. validateToken 으로 토큰 유효성 검사
-////        if (token != null && jwtTokenProvider.validateToken(token)) {
-////            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장
-////            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-////            SecurityContextHolder.getContext().setAuthentication(authentication);
-////        }
-//        chain.doFilter(request, response);
-//    }
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = resolveToken(request);
