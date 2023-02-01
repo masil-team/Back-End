1b6e00ede43cc4ff01e84411fdb07e6040bba143
JUN KIM <67573836+wnsrl1228@users.noreply.github.com>
2023-02-01 16:29:46 +0900

    bug : cors버그수정 (#48)

--- src/main/java/com/masil/global/config/security/SecurityConfig.java
+++ src/main/java/com/masil/global/config/security/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
