0c3f3c6513fbfaf5f55e777ccea637bece10a98d
wnsrl <wnsrl981228@gmail.com>
2023-03-05 23:31:32 +0900

    test : update post api docs about file

--- src/main/java/com/masil/domain/post/dto/PostsElementResponse.java
+++ src/main/java/com/masil/domain/post/dto/PostsElementResponse.java
@@ -8,7 +8,6 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -27,14 +26,15 @@ public class PostsElementResponse {
     private Boolean isScrap;
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
-    private List<FileResponse> files;
+    private FileResponse thumbnail;
 
     public static PostsElementResponse of(Post post) {
-        List<FileResponse> filesResponse = new ArrayList<>();
 
-        for (PostFile postFile : post.getPostFiles()) {
-            filesResponse.add(FileResponse.of(postFile));
-        }
+        FileResponse thumbnail = null;
+        List<PostFile> postFiles = post.getPostFiles();
+
+        if (!postFiles.isEmpty())
+            thumbnail = FileResponse.of(postFiles.get(0));
 
         return PostsElementResponse.builder()
                 .id(post.getId())
@@ -50,7 +50,7 @@ public class PostsElementResponse {
                 .isScrap(post.getIsScrap())
                 .createDate(post.getCreateDate())
                 .modifyDate(post.getModifyDate())
-                .files(filesResponse)
+                .thumbnail(thumbnail)
                 .build();
     }
 }
